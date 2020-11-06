15677f07ad6b7c0c212016ca28ee1de0b7062a84
Oliver <olihui@gmx.de>
2020-11-06 23:59:43 +0100

    TEMP: Covid VAT rates

--- windows/katalog_upload.php
+++ windows/katalog_upload.php
@@ -631,10 +631,12 @@ function upload_bnn( $katalogformat ) {
 
     switch( trim( $splitline[33] ) ) {
       case '1':
-        $mwst = "7.00";
+        // corona special:
+        $mwst = "5.00";
         break;
       case '2':
-        $mwst = "19.00";
+        // corona special:
+        $mwst = "16.00"; 
         break;
       default:
         break;
