4ae4376860290c7ff25c7e43485de15e07a42482
Oliver Gondring <og-dev@gmx.de>
2022-02-08 01:07:31 +0100

    fixup twig configuration

--- src/twiggie.php
+++ src/twiggie.php
@@ -3,8 +3,9 @@ require_once(__DIR__ . '/vendor/autoload.php');
 $loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/templates');
 $twig = new \Twig\Environment(
     $loader,
-    ['cache' => __DIR__ . 'templates/cache',]
+    // this has to be adjusted for deployment (which value would work?)
+    // make it a foodsoft ENV set in config.php
+    ['cache' => '/tmp/twig-template-cache',]
 );
 
 echo $twig->render('hello.twig', ['pageTitle' => "The world is mine!"]);
-echo "OK!";
